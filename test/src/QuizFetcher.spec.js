cda2cbf4aa726119ea21aefc233d8b4a2c8ea964
Cheekyfunkymonkey <shigechu@gmail.com>
2019-04-16 14:25:38 +1200

    Edit QuizFetcher.spec.js

--- test/src/QuizFetcher.spec.js
+++ test/src/QuizFetcher.spec.js
@@ -1,57 +1,111 @@
 const assert = require("power-assert");
 const QuizFetcher = require("../../src/QuizFetcher");
 
-describe("QuizFetcher.js test", () => {
-  it("should have results property", () => {
-    return QuizFetcher.fetch().then(data => {
-      assert.equal(data.hasOwnProperty("results"), true);
+describe("QuizFetcherのクラス", () => {
+  describe("fetchメソッドの挙動確認", () => {
+    it("fetchメソッドという名前のクラスメソッドをもつ", () => {
+      assert.equal(QuizFetcher.hasOwnProperty("fetch"), true);
+      assert.equal(typeof QuizFetcher.fetch, "function");
     });
-  });
-  it("should be an array that has 10 values", () => {
-    return QuizFetcher.fetch()
-      .then(data => data.results)
-      .then(results => {
-        assert.equal(results.length, 10);
+
+    it("【async/await版】fetchメソッドの戻り値型チェック", async () => {
+      const data = await QuizFetcher.fetch();
+      const results = await data.results;
+
+      //results プロパティを持つ
+      assert.equal(data.hasOwnProperty("results"), true);
+      //results プロパティはは10件データをもつ配列である
+      assert.equal(results.length, 10);
+      //results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
+      results.forEach(result => {
+        assert.equal(typeof result, "object");
+        assert.equal(result.hasOwnProperty("category"), true);
+        assert.equal(result.hasOwnProperty("type"), true);
+        assert.equal(result.hasOwnProperty("difficulty"), true);
+        assert.equal(result.hasOwnProperty("question"), true);
+        assert.equal(result.hasOwnProperty("correct_answer"), true);
+        assert.equal(result.hasOwnProperty("incorrect_answers"), true);
       });
-  });
-  it("all the values in results array should be objects", () => {
-    return QuizFetcher.fetch()
-      .then(data => data.results)
-      .then(results => {
-        results.forEach(result => {
-          assert.equal(typeof result, "object");
+      //typeof を使って、書くプロパティ値の型をテストする
+      results.forEach(result => {
+        assert.equal(typeof result.category, "string");
+        assert.equal(typeof result.type, "string");
+        assert.equal(typeof result.difficulty, "string");
+        assert.equal(typeof result.question, "string");
+        assert.equal(typeof result.correct_answer, "string");
+        assert.equal(Array.isArray(result.incorrect_answers), true);
+        result.incorrect_answers.forEach(incorrect_answer => {
+          assert.equal(typeof incorrect_answer, "string");
         });
       });
-  });
-  it("the objects in results array should have category, type, difficulty, question, correct_answer and incorrect_answers properties", () => {
-    return QuizFetcher.fetch()
-      .then(data => data.results)
-      .then(results => {
-        results.forEach(result => {
-          assert.equal(result.hasOwnProperty("category"), true);
-          assert.equal(result.hasOwnProperty("type"), true);
-          assert.equal(result.hasOwnProperty("difficulty"), true);
-          assert.equal(result.hasOwnProperty("question"), true);
-          assert.equal(result.hasOwnProperty("correct_answer"), true);
-          assert.equal(result.hasOwnProperty("incorrect_answers"), true);
-        });
-      });
-  });
-  it("category, type, difficulty, question and correct_answer properties should be string. incorrect_answers should be an array with three strings", () => {
-    return QuizFetcher.fetch()
-      .then(data => data.results)
-      .then(results => {
-        results.forEach(result => {
-          assert.equal(typeof result.category, "string");
-          assert.equal(typeof result.type, "string");
-          assert.equal(typeof result.difficulty, "string");
-          assert.equal(typeof result.question, "string");
-          assert.equal(typeof result.correct_answer, "string");
-          assert.equal(Array.isArray(result.incorrect_answers), true);
-          result.incorrect_answers.forEach(incorrect_answer => {
-            assert.equal(typeof incorrect_answer, "string");
+    });
+
+    it("【Promise版】fetchメソッドの戻り値型チェック", () => {
+      return QuizFetcher.fetch()
+        .then(data => {
+          //results プロパティを持つ
+          assert.equal(data.hasOwnProperty("results"), true);
+          return data.results;
+        })
+        .then(results => {
+          //results プロパティはは10件データをもつ配列である
+          assert.equal(results.length, 10);
+          //results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
+          results.forEach(result => {
+            assert.equal(typeof result, "object");
+            assert.equal(result.hasOwnProperty("category"), true);
+            assert.equal(result.hasOwnProperty("type"), true);
+            assert.equal(result.hasOwnProperty("difficulty"), true);
+            assert.equal(result.hasOwnProperty("question"), true);
+            assert.equal(result.hasOwnProperty("correct_answer"), true);
+            assert.equal(result.hasOwnProperty("incorrect_answers"), true);
+          });
+          //typeof を使って、書くプロパティ値の型をテストする
+          results.forEach(result => {
+            assert.equal(typeof result.category, "string");
+            assert.equal(typeof result.type, "string");
+            assert.equal(typeof result.difficulty, "string");
+            assert.equal(typeof result.question, "string");
+            assert.equal(typeof result.correct_answer, "string");
+            assert.equal(Array.isArray(result.incorrect_answers), true);
+            result.incorrect_answers.forEach(incorrect_answer => {
+              assert.equal(typeof incorrect_answer, "string");
+            });
           });
         });
+    });
+
+    it("【コールバック(done)版】fetchメソッドの戻り値型チェック", done => {
+      const data = QuizFetcher.fetch();
+      const results = data.results;
+      done();
+
+      //results プロパティを持つ
+      assert.equal(data.hasOwnProperty("results"), true);
+      //results プロパティはは10件データをもつ配列である
+      assert.equal(results.length, 10);
+      //results プロパティの配列の中身は全てオブジェクトで、次のプロパティを持つ
+      results.forEach(result => {
+        assert.equal(typeof result, "object");
+        assert.equal(result.hasOwnProperty("category"), true);
+        assert.equal(result.hasOwnProperty("type"), true);
+        assert.equal(result.hasOwnProperty("difficulty"), true);
+        assert.equal(result.hasOwnProperty("question"), true);
+        assert.equal(result.hasOwnProperty("correct_answer"), true);
+        assert.equal(result.hasOwnProperty("incorrect_answers"), true);
       });
+      //typeof を使って、書くプロパティ値の型をテストする
+      results.forEach(result => {
+        assert.equal(typeof result.category, "string");
+        assert.equal(typeof result.type, "string");
+        assert.equal(typeof result.difficulty, "string");
+        assert.equal(typeof result.question, "string");
+        assert.equal(typeof result.correct_answer, "string");
+        assert.equal(Array.isArray(result.incorrect_answers), true);
+        result.incorrect_answers.forEach(incorrect_answer => {
+          assert.equal(typeof incorrect_answer, "string");
+        });
+      });
+    });
   });
 });
